6fbbee1c7e9ac9bd3a545629ec3e69be32a92390
Kev575 <phoenix.games.minecraft@gmail.com>
2016-02-08 17:28:56 +0100

    Update entityNoAI.java

--- spigot/entityNoAI.java
+++ spigot/entityNoAI.java
@@ -3,6 +3,11 @@ public boolean hasAI(org.bukkit.entity.Entity ent) {
 	net.minecraft.server.v1_8_R3.Entity entity = ((net.minecraft.server.v1_8_R3.Entity)((CraftEntity)ent).getHandle());
 	return entity.getNBTTag() != null && !entity.getNBTTag().isEmpty() && entity.getNBTTag().hasKey("NoAI");
 }
-public void doStuff(//listener or whatever) {
+public void doStuff(Entity entity) {
 	boolean ai = hasAI(entity);
+	if (ai) {
+		// By happy xD
+	} else {
+		// Crryyyy xD
+	}
 }
